58ae6676be7f15a9ce25a590dcf2b25b87bae09f
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-03 21:31:35 +0300

    Массивы. Slice, splice, concat, reverse (42)

--- app.js
+++ app.js
@@ -1,28 +1,46 @@
 /**
  * Angol. 2024.07.03
  * Массивы
- * Поиск элемента
+ * Slice, splice, concat, reverse (42)
  */
 
-const roles = ['user', 'admin', 'manager', 'admin'];
+const roles = ['user', 'admin', 'manager', 'superuser'];
 
-const elIndex = roles.indexOf('admin'); // метод inexOf ищет первое вхождение индекса
-console.log(elIndex); // 1
+// Slice
+const res = roles.slice(2); // исходный массив не модифицрует
+// отрезает кусок начиная с указанного элемента до конца массива
+console.log(res);
 
-const elIndex2 = roles.indexOf('superuser'); // метод inexOf ищет первое вхождение индекса
-console.log(elIndex2); // -1 - индек не найден
+const res2 = roles.slice(2, 3); // отрезает кусок массива начиная с элемента в первом аргументе по элемент во втором элементе, но не включая его
+console.log(res2);
 
-// условие
-if (roles.indexOf('admin') >= 0) {
-    console.log('Доступ есть');
-}
+const res3 = roles.slice(-1);
+const res4 = roles[roles.length - 1] // отрезает кусок массива начиная с конца массива в обратном направлении
+console.log(res3);
+console.log(res4);
 
-// includes возвращает булево
-console.log(roles.includes('admin')); // true
-console.log(roles.includes('superuser')); // false
+const res5 = roles.slice(1, -2); // отрезает кусок массива начиная с элемента в первом аргументе по элемент во втором элементе с конца массива, но не включая его
+console.log(res5);
+console.log(roles); // исходный код не можифицирован.
 
-// условие
-if (roles.includes('admin')) {
-    console.log('Доступ есть');
-}
+// Splice
+// const res6 = roles.splice(2);
+// console.log(res6);
+// console.log(roles); // исходный код можифицирован. Обрезан! Вырезаны указанные в splice элементы.
 
+// const res6 = roles.splice(2, 2); // выбирает из исходного массива элементы начиная с индекса 2, 2 элемента
+// console.log(res6);
+// console.log(roles); // исходный код можифицирован. Обрезан! Вырезаны указанные в splice элементы.
+
+// const res6 = roles.splice(-1); // выбирает из исходного массива элементы начиная с конца массива
+// console.log(res6);
+// console.log(roles); // исходный код можифицирован. Обрезан! Вырезаны указанные в splice элементы.
+
+const res8 = roles.reverse(); // переворачивает массивы
+console.log(res8);
+console.log(roles); // модифицирует исходный массив
+
+const newRoles = ['sysadmin', 'developer'];
+const res9 = roles.concat(newRoles);
+console.log(res9);
+console.log(roles); // исходный массив не модифицирован.
