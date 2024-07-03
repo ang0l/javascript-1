ff9cb6990a683a780f70f9a8ec7fa08d017fbec6
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-03 15:44:44 +0300

    Тернарные операторы (24)

--- app.js
+++ app.js
@@ -1,60 +1,37 @@
 /**
  * Angol. 2024.07.03
  * Управление потоком
- * Switch (23)
+ * Тернарные операторы (24)
  */
 
-const role = 'manager';
+const bmwX3Price = 100000;
+const fordFocusPrice = 10000;
+const budget = 20000;
 
-// Сравнение с if else
-// if (role === 'manager') {
-//     console.log('Менеджер');
-// } else if (role === 'admin') {
-//     console.log('Администратор');
-// } else if (role === 'ceo') {
-//     console.log('СЕО');
-// } else {
-//     console.log('Мы тебя не знаем');
-// }
-
-// сам switch
-switch (role) {
-    case 'manager': // role === 'manager' (именно строгое равенство)
-        console.log('Менеджер');
-        break;
-    case 'admin':
-        console.log('Администратор');
-        break;
-    case 'ceo':
-        console.log('СЕО');
-        break;
-    default:
-        console.log('Мы тебя не знаем');
+// Условие if else
+//
+// Переменную необходимо объявить до проверки условия
+// При объявлениеи внутри условия переменной,
+// ее не видно снаружи условия.
+let message;
+if (budget > bmwX3Price) {
+    message = 'BMW';
+} else {
+    message= 'Велосипед';
 }
 
-// Объединение кейсов
-switch (role) {
-    case 'manager':
-    case 'admin':
-        console.log('Не руководитель');
-        break;
-    case 'ceo':
-        console.log('Руководитель');
-        break;
-    default:
-        console.log('Мы тебя не знаем');
-}
+console.log(`Я хочу купить ${message}`);
 
-const num = 1;
+// Тернарный оператор
+// 10 > 0 ? console.log('Больше 0') : console.log('Меньше 0');
+const str = 10 > 0 ? 'Больше 0' : 'Меньше 0';
+console.log(str);
 
-// Проверка на разные сравнения
-switch(true) {
-    case num > 0: // true === num > 0
-        console.log('Положительный');
-        break;
-    case num < 0: // true === num < 0
-        console.log('Отрицательный');
-        break;
-    default:
-        console.log('Ноль!');
-}
+console.log(`Я хочу купить ${budget > bmwX3Price ? 'BMW' : budget > fordFocusPrice ? 'Ford' : 'Велосипед'}`);
+
+// Вариант с переменной
+let car = budget > bmwX3Price
+    ? 'BMW'
+    : budget > fordFocusPrice ? 'Ford' : 'Велосипед';
+
+console.log(`Я хочу купить ${car}`);
