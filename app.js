a05b33e6c4354b77c461d461dfb742ab74cdb469
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-03 16:32:04 +0300

    Функции. Функции в Функциях (37)

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 /**
  * Angol. 2024.07.03
  * Функции
- * Условия в функциях
+ * Функции в функциях (37)
  */
 
-function canAccessWebSite(age) {
-    if (age < 18) {
-        return 'Нет';
-    }
-    return 'Да';
-    // в этом условии не нужен else, т.к. попадая в условие функция
-    // прекращаетсвою работу и return true не сработает.
-}
-console.log(canAccessWebSite(18));
+const KG_IN_USD = 7;
+const KM_IN_USD = 5;
 
-const canAccessWebSite2 = age => age < 18 ? 'Нет' : 'Да';
-console.log(canAccessWebSite2(16));
+function calculateW(present) {
+    return present * KG_IN_USD;
+}
+
+function calculateKM(distance) {
+    return distance * KM_IN_USD;
+}
+
+function getExhangePrice(present1, present2, distance) {
+    // Расчет без дополнительных функций
+    // const price1 = present1 * KG_IN_USD;
+    // const price2 = present2 * KG_IN_USD;
+    // const distancePrice = distance * KM_IN_UDS;
+    const price1 = calculateW(present1);
+    const price2 = calculateW(present2);
+    const distancePrice = calculateKM(distance);
+    return price1 + price2 + distancePrice;
+}
+console.log(getExhangePrice(1, 2, 10));
