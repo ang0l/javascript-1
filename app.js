09f2d7fcd6204434dbf6cd7180769bd817a87ca9
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-03 15:50:17 +0300

    Оператор нулевого слияния (29)

--- app.js
+++ app.js
@@ -1,29 +1,16 @@
 /**
  * Angol. 2024.07.03
  * Булева логика
- * Операторы с другими типами (28)
+ * Оператор нулевого слияния (29)
  */
 
-// Выведет первый операнд если он true
-console.log('Вася' || 'Олег'); // Вася
-console.log(false || 'Олег'); // Олег
-console.log('Вася' || false); // Вася
-console.log(false || false); // false
+let userName = 'Вася';
+// Ноль, пустая строка, null, undefinde, false
+// в переменной userName
+// выведут: Default Username
+console.log(userName || 'Default Username');
 
-// Выведет второй операнд если первый true
-console.log('Вася' && 'Олег'); // Олег
-
-// Веведет false если какой-нибудь операнд - false
-console.log(false && 'Олег'); // false
-console.log('Вася' && false); // false
-console.log(false || false); // false
-
-let a;
-const userName = a || 'Петя';
-// Т.к. переменная a не определена, то выведет 'Петя'
-// если переменную a определить, то выведет значение переменной a
-console.log(userName);
-
-const isAdmin = true;
-const fileName = isAdmin && 'file.mp4';
-console.log(fileName);
+// null и undefinde
+// в переменной userName
+// выведут: Default Username
+console.log(userName ?? 'Default Username');
