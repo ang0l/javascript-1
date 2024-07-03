166572fbe0cfefc649835549f08bb48c344f0a5c
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-03 15:38:16 +0300

    If else (20)

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 /**
- * Angol 2024.07.02
- * Основы
- * False из других типов (19)
+  * Angol. 2024.07.03
+  * Управление потоком
+  * If else (20)
  */
 
-console.log(Boolean(0));
-console.log(Boolean(''));
-console.log(Boolean(undefined));
-console.log(Boolean(null));
-console.log(Boolean(Number('str')));
-console.log(Boolean(NaN));
+const money = 10;
+
+if (money > 50) {
+    console.log('Может купить наш продукт');
+} else if (money > 5) {
+    console.log('Куплен mini продукт');
+} else {
+    console.log('Не хватает баланса');
+}
+
+console.log('Итог');
