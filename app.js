620d43882d6244a7caf6ed367dc2906f0d3a9581
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-03 17:27:08 +0300

    Функции. Упражнение - Кредит на MacBook (38)

--- app.js
+++ app.js
@@ -1,28 +1,68 @@
 /**
  * Angol. 2024.07.03
  * Функции
- * Функции в функциях (37)
+ * Упражнение - Кредит на MacBook
  */
 
-const KG_IN_USD = 7;
-const KM_IN_USD = 5;
+// Пользователь:
+// - Возраст
+// - Наличие работы
+// - Деньги
+// Нужно проверить может ли он купить новый MacBook за 2000$?
+// Он может брать не только свои деньги, но и взять кредит.
+// Ему дадут 500$, только если ему больше 24-х лет и он
+// имеет работу, 100$ если ему просто больше 24-х лет и 0 в
+// ином случае.
+// Напишите функцию, которая принимает данные пользователя
+// и товара и возвращает true или false;
 
-function calculateW(present) {
-    return present * KG_IN_USD;
+// // Мое решение
+// let age = 24;
+// let isWork = true;
+// let cach = 2200;
+// const priceMacBook = 2000;
+
+// function creditOnMacBook(age, work) {
+//     if (age > 24 && work) {
+//         return 500;
+//     } else if (age > 24) {
+//         return 100;
+//     }
+//     return 0;
+// }
+
+// function canBuyMacBook (age, work, cach, priceMacBook) {
+
+//     let cachAndCredit = cach + creditOnMacBook(age, work);
+//     // console.log(cachAndCredit);
+
+//     if (priceMacBook <= cachAndCredit) {
+//         return true;
+//     }
+//     return false;
+
+// }
+// console.log(canBuyMacBook(age, isWork, cach, priceMacBook));
+
+// Решение преподавателя.
+function computeCredit(age, hasJob = false) {
+    switch (true) {
+        case age > 24 && hasJob:
+            return 500;
+        case age > 24:
+            return 100;
+        default:
+            return 0;
+    }
 }
 
-function calculateKM(distance) {
-    return distance * KM_IN_USD;
+// агрументы по умольчанию необходимо устанавливать в конце
+// иначе не работает и нет, соответственно, смысла
+// function canBuy(productPrice, age, hasJob = false, money) {
+function canBuy(productPrice, age, money, hasJob = false) {
+    const creditMoney = computeCredit(age, hasJob);
+    return productPrice <= money + creditMoney;
 }
+console.log(canBuy(2000, 25, 1900, false));
 
-function getExhangePrice(present1, present2, distance) {
-    // Расчет без дополнительных функций
-    // const price1 = present1 * KG_IN_USD;
-    // const price2 = present2 * KG_IN_USD;
-    // const distancePrice = distance * KM_IN_UDS;
-    const price1 = calculateW(present1);
-    const price2 = calculateW(present2);
-    const distancePrice = calculateKM(distance);
-    return price1 + price2 + distancePrice;
-}
-console.log(getExhangePrice(1, 2, 10));
+// почти тоже самое, но у преподавателя более красивее
