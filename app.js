b3c127562fc4f6f9025a3c5b4c51b92df0bd507b
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-03 15:47:01 +0300

    Упражнение - Проверка робота (25)

--- app.js
+++ app.js
@@ -1,37 +1,55 @@
 /**
  * Angol. 2024.07.03
  * Управление потоком
- * Тернарные операторы (24)
+ * Упражнение - Проверка робота (25)
  */
 
-const bmwX3Price = 100000;
-const fordFocusPrice = 10000;
-const budget = 20000;
+// Методом prompt получите ответ пользователя
+// на запрос "Сколько будет 7 + или - 15?". Если ответ верен
+// выведите в консоле "Успех", если нет - "Вы робот!",
+// а ели он введет "Я не робот", то тоже "Успех".
 
-// Условие if else
+// Мой вариант решения:
+// let answer = prompt('Сколько будет 7 + или - 15?');
+// let result;
+// 
+// switch (true) {
+//     case answer == -8:
+//     case answer == 22:
+//     case answer == 'Я не робот':
+//         result = 'Успех';
+//         break;
+//     default:
+//         result = 'Вы робот';
+// }
 //
-// Переменную необходимо объявить до проверки условия
-// При объявлениеи внутри условия переменной,
-// ее не видно снаружи условия.
-let message;
-if (budget > bmwX3Price) {
-    message = 'BMW';
-} else {
-    message= 'Велосипед';
+// console.log(result);
+
+// Решение преподавателя:
+// Вариант 1:
+const res = prompt('Сколько будет 7 + или - 15?');
+
+switch(true) {
+    case res === 'Я не робот':
+    case Number(res) === 22:
+    case Number(res) === -8:
+        console.log('Успех');
+        break;
+    default:
+        console.log('Вы робот');
 }
 
-console.log(`Я хочу купить ${message}`);
-
-// Тернарный оператор
-// 10 > 0 ? console.log('Больше 0') : console.log('Меньше 0');
-const str = 10 > 0 ? 'Больше 0' : 'Меньше 0';
-console.log(str);
-
-console.log(`Я хочу купить ${budget > bmwX3Price ? 'BMW' : budget > fordFocusPrice ? 'Ford' : 'Велосипед'}`);
-
-// Вариант с переменной
-let car = budget > bmwX3Price
-    ? 'BMW'
-    : budget > fordFocusPrice ? 'Ford' : 'Велосипед';
-
-console.log(`Я хочу купить ${car}`);
+// Вариант 2:
+if (res === 'Я не робот' ) {
+    console.log('Успех');
+} else {
+    const resNum = Number(res);
+    switch(resNum) {
+        case 22:
+        case -8:
+            console.log('Успех');
+            break;
+        default:
+            console.log('Вы робот!');
+    }
+}
