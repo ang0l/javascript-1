b35de2dc009c1067cb4495582e7520fd13b1812d
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-04 21:30:28 +0300

    Массивы. Упражнение - Функция разбора URL (48)

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 /**
  * Angol. 2024.07.03
  * Массивы
- * Rest оператор
+ * Упражнение - Функция рабора URL (48)
  */
 
-const data = [1, 2, 3, 4, 5, 6];
-// rest оператор (...) можно использовать только в конце
-// т.к. он собирает оставшуюся часть массива
-// в начале или середине он работать не будет. будет ошибка
-const [one, two, ...others] = data;
-console.log(one, two, others); // 1 2 (4) [3, 4, 5, 6]
+// Дан произвольный url -
+// 'https://purplechool.ru/course/javascript'
+// Нужно сделать функцию, которая выводит в консоль:
+// - Протокол (https)
+// - Доменное имя (purpleschool.ru)
+// - Путь внутри сайта (/course/javascript)
+
+const url = 'https://purplechool.ru/course/javascript';
+
+function getUrlParts(url) {
+    const [protocol, _, host, ...path] = url.split('/');
+    console.log(protocol, host, path);
+    console.log(`Протокол: ${protocol.split(':')[0]}`);
+    console.log(`Доменное имя: ${host}`);
+    console.log(`Путь внутри сайта: /${path.join('/')}`);
+}
+// в даннной функции нет проверки на правильность задания url
+
+getUrlParts(url);
