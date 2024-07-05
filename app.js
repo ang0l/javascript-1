59693e129681c0c64311373a6bcd94fc8ef31b6d
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-05 10:06:53 +0300

    Функции высшего порядка. Возврат функций (58)

--- app.js
+++ app.js
@@ -1,34 +1,20 @@
 /**
  * Angol. 2024.07.05
  * Функции высшего порядка
- * Callback (57)
+ * Возврат функций (58)
  */
 
-// функциb первого класса
-function add(a, b) {
-    return a + b;
+function power(pow) {
+    return function (num) {
+        return num ** pow;
+    }
 }
 
-function subtract(a, b) {
-    return a - b;
-}
+const powerOfTwo = power(2);
+console.log(powerOfTwo(5));
+console.log(powerOfTwo(10));
 
-function power(a, b) {
-    return a ** b;
-}
+const powerOfTree = power(3);
+console.log(powerOfTree(5));
 
-// функция высшего порядка
-function calculate(a, b, fn) {
-    console.log(fn.name); // выводим имя функции
-    const res = fn(a, b);
-    return res;
-}
-
-let res = calculate(3, 5, add);
-console.log(res);
-
-res = calculate(3, 5, subtract);
-console.log(res);
-
-res = calculate(3, 5, power);
-console.log(res);
+console.log(power(5)(4));
