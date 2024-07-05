d5471a5ca2236392bdad0be11a4221d77bd29279
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-05 12:01:41 +0300

    Итерации в массивах. Map (61)

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 /**
  * Angol. 2024.07.05
  * Итерации в массивах
- * ForEach (58)
+ * Map (61)
  */
 
-const score = [5, 10, 0, 15];
+// const score = [5, 10, 0, 15];
 
-// for (const[i, el] of score.entries()) { // Метод entries возвращает массив с индеком и значением элемента массива.
-//     console.log(`Раунд ${i + 1}:  ${el}`);
-// }
+// // for (const[i, el] of score.entries()) { // Метод entries возвращает массив с индеком и значением элемента массива.
+// //     console.log(`Раунд ${i + 1}:  ${el}`);
+// // }
 
-// score.forEach(function(el) {
-//     console.log(`Раунд:  ${el}`);
+// score.forEach((scoreEl, i) => { // есть еще третий аргумент, который является самим исходным массивом.
+//     console.log(`Раунд ${i + 1}:  ${scoreEl}`);
 // })
-//
-// вариант стрелочной функции
-score.forEach((el, i) => { // аргументы: елемент, индекс
-    console.log(`Раунд ${i + 1}:  ${el}`);
-})
-// особенностью метода forEach является то, что нельзя отменить итерирование break-ом.
+
+const transactionInUSD = [10, -7, 50, -10, 100];
+
+// конвертация USD в RUB
+
+// процедурный стиль:
+const transactionInRUB = [];
+for (const transaction of transactionInUSD) {
+    transactionInRUB.push(transaction * 60);
+}
+console.log(transactionInUSD);
+console.log(transactionInRUB);
+
+// функциональный стиль:
+const transactionInRUB2 = transactionInUSD.map((transaction) => transaction * 60);
+console.log(transactionInUSD);
+console.log(transactionInRUB2);
