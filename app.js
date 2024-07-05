d26e695c081be01fab0b135bd72a5364ba73302c
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-05 09:56:46 +0300

    Функции высшего порядка. Callback (57)

--- app.js
+++ app.js
@@ -1,113 +1,34 @@
 /**
  * Angol. 2024.07.05
- * Циклы
- * Упражнение - Расчет итогвого баланса (55)
+ * Функции высшего порядка
+ * Callback (57)
  */
 
-// Есть выгрузка операций пользователя
-// const operations = [1000, -700, 300, -500, 10000];
-// а так же начальный баланс в $100
-// Необходимо сделать функции расчета:
-// - Итогвого баланса
-// - Наличия отрицательного баланса (если после очередной операции
-//     баланс < 0, то выдавать false)
-// - Расчета среднего расхода и среднего дохода
-
-const operations = [1000, -700, 300, -500, 10000];
-
-// // Мое решение:
-// const beginBalance = 100;
-
-// function total(begin, operations) {
-//     let balance = begin;
-//     for (let operation of operations) {
-//         balance += operation;
-//     }
-//     return balance;
-// }
-
-// function negativeBalance (begin, operations) {
-//     let balance = begin;
-//     for (let operation of operations) {
-//         balance += operation;
-//         if (balance < 0) {
-//             return false;
-//         }
-//     }
-//     return true;
-// }
-
-// function average(operations) {
-//     let quantitPuls = 0;
-//     let quantitMinus = 0;
-//     let balancePlus = 0;
-//     let balanceMinus = 0;
-
-//     for (let operation of operations) {
-//         if (operation > 0) {
-//             balancePlus += operation;
-//             quantitPuls++;
-//         } else if (operation < 0) {
-//             balanceMinus += operation;
-//             quantitMinus++;
-//         }
-//     }
-//     console.log(balancePlus /= quantitPuls);
-//     console.log(balanceMinus /= quantitMinus);
-// }
-
-// console.log(total(beginBalance, operations));
-// console.log(negativeBalance(beginBalance, operations));
-// average(operations);
-
-// Решение преподавателя:
-const startingBalance = 100;
-
-// Эта функция такая же как у меня.
-function getBalance(arrayOfOperations, initialBalance) {
-    let balance = initialBalance;
-    for (const element of arrayOfOperations) {
-        balance += element;
-    }
-    return balance;
+// функциb первого класса
+function add(a, b) {
+    return a + b;
 }
-console.log(getBalance(operations, startingBalance));
 
-// Эта функция отличается от моей наличием переменной isOk и выходом из цикла с помощью break.
-function checkOperations(arrayOfOperations, initialBalance) {
-    let balance = initialBalance;
-    let isOk = true;
-    for (const element of arrayOfOperations) {
-        balance += element;
-        if (balance < 0) {
-            isOk = false;
-            break;
-        }
-    }
-    return isOk;
+function subtract(a, b) {
+    return a - b;
 }
-console.log(checkOperations(operations, startingBalance));
 
-// Эта функция отличается от моей возвратом массива.
-// у меня была такая мысль, но вывел данные в самой функции.
-function averageOperations(arrayOfOperations) {
-    let positiveCount = 0;
-    let negativeCount = 0;
-    let positiveSum = 0;
-    let negativeSum = 0;
-
-    for (const element of arrayOfOperations) {
-        if (element > 0 ) {
-            positiveCount++;
-            positiveSum += element;
-        }
-        if (element < 0 ) {
-            negativeCount++;
-            negativeSum += element;
-        }
-    }
-    return [positiveSum / positiveCount, negativeSum / negativeCount];
+function power(a, b) {
+    return a ** b;
 }
-console.log(averageOperations(operations));
 
-// далее будем учить и писать такие конструкции более красивее и короче.
+// функция высшего порядка
+function calculate(a, b, fn) {
+    console.log(fn.name); // выводим имя функции
+    const res = fn(a, b);
+    return res;
+}
+
+let res = calculate(3, 5, add);
+console.log(res);
+
+res = calculate(3, 5, subtract);
+console.log(res);
+
+res = calculate(3, 5, power);
+console.log(res);
