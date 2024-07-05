41ab121298a743c21db4517670e0ad2c7321c06d
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-05 07:51:13 +0300

    Циклы. Циклы For of и For in (54)

--- app.js
+++ app.js
@@ -1,42 +1,20 @@
 /**
  * Angol. 2024.07.05
  * Циклы
- * Цикл While (52)
+ * Циклы For of и For in (54)
  */
 
-for (let i = 1; i < 5 ; i++) {
-    console.log(`Вывод - ${i}`);
-}
-
-let i = 1;
-while (i < 5) {
-    console.log(`Вывод - ${i}`);
-    i++;
-}
-
-//////////////
-console.log('-----------------');
 const arr = [1, 4, 8, 7];
 
 for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 5) {
-        break;
-    }
     console.log(arr[i]);
 }
 
-// Сначала проверка условия, потом выполнение кода.
-// В зависимости от условия цикл можен не выполниться ни разу.
-i = 0;
-while (arr[i] <= 5 && i < arr.length) {
-    console.log(arr[i]);
-    i++;
+for (let element of arr) {
+    console.log(element);
 }
 
-// Сначала выполнение кода, потом проверка условия.
-// Этот цикл будет выполняться как минимум 1 раз.
-let j = 0;
-do {
-    console.log(j);
-    j++;
-} while (j < 0)
+for (let index in arr) {
+    console.log(index);
+    console.log(arr[index]);
+}
