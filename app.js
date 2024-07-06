3509dcafd0452058d8d8b2c69384118e4fff71d6
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-06 10:31:14 +0300

    Итерация в массивах. flat и flatMap (68)

--- app.js
+++ app.js
@@ -1,37 +1,20 @@
 /**
  * Angol. 2024.07.06
  * Итерации в массивах
- * Упражнение - Реализация Some (66)
- */
-
-const arr = [2, 4, 4, 10, 20];
-
-// Мое решение в 2 вариантах
-// Функция на цикле
-function someOnCycle(arr, arrEl) {
-    res = false;
-    for (const el of arr) {
-        if (el === arrEl) {
-            res = true;
-        }
-    }
-    return res;
-}
-console.log(someOnCycle(arr, 4));
-
-// Функция на findIndex
-function someOnFindIndex(arr, arrEl) {
-    return arr.findIndex(el => el === arrEl) > -1;
-}
-console.log(someOnFindIndex(arr, 5));
-
-// Решение преподавателя
-function some(array, element) {
-    const res = array.find(el => el === element);
-    return res == undefined ? false : true;
-}
-console.log(some(arr, 22));
-
-// а теперь
-// реализованный метод some в JS
-console.log(arr.some(el => el === 4));
+ * flat и flatMap (68)
+ */ 
+const prices = [[2, 4], [3, 4], [10, [20, 50]]];
+ 
+// метод flat делает массив "плоским".
+// Т.е. из многомерного массива делает одномерный.
+// глубина вложения передается аргументом, по умолчанию - 1
+console.log(prices.flat()); // (6) [2, 4, 3, 4, 10, Array(2)]
+console.log(prices.flat(2)); // (7) [2, 4, 3, 4, 10, 20, 50]
+ 
+// flatMap - последовательное применение Map, а потом flat
+// новый метод. но в большинстве случаев лучше использовать
+// сначала map, потом flat:
+// prices.map(el => el.concat([2])).flat()
+// но в некоторых случаях это удобней, мы записываем это одним методом
+console.log(prices.flatMap(el => el.concat([1]))); // добавили елемент со значением 1 между элементами. результат ниже:
+// (9) [2, 4, 1, 3, 4, 1, 10, Array(2), 1]
