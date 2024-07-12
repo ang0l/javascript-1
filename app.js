b0259a7bca0b972a11bcccabf7f4fe05526f33f0
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-12 06:26:08 +0300

    Объекты. Упражнение преобразование объектов (82)

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
 /**
  * Angol. 2024.07.11
  * Объекты
- * Упражнение - сортировка пользователей (81)
+ * Упражнение - преобразование объектов (82)
  */
 
-// отсортировать пользователей по возрасту
+// Преобразовать пользователей до вида
+// { fullName: 'Вася Пупкин', skillNum: 2 }
 
 const users = [
-    {name: 'Вася', age: 30},
-    {name: 'Катя', age: 18},
-    {name: 'Аня',  age: 40},
-    {name: 'Петя', age: 25}
+    {
+        name: 'Вася',
+        surname: 'Пупкин',
+        age: 30,
+        skills: ['Разраотка', 'DevOps']
+    },
+    {
+        name: 'Катя',
+        surname: 'Белова',
+        age: 18,
+        skills: ['Дизайн']
+    },
 ];
 
-console.log(users.sort((a, b) => a.age - b.age)); // массив объектов отсортирован по возрасту
+// Мое решение:
+const usersMod = users.map((obj) => {
+    return {
+        fullName: `${obj.name} ${obj.surname}`,
+        skillNum: obj.skills.length
+    }
+});
+console.log(usersMod);
+
+// Решение преподавателя
+const userData = users.map(user => {
+    return {
+        fullName: `${user.name} ${user.surname}`,
+        skillNum: user.skills.length
+    }
+});
+console.log(userData);
+
+// название переменных...
