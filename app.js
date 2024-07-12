81600da84bf0bc10e6256741ec8ccc8763038fc9
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-12 21:05:36 +0300

    Enhance object literals (85)

--- app.js
+++ app.js
@@ -1,46 +1,22 @@
 /**
  * Angol. 2024.07.12
  * Объекты
- * Упражнение - кошелек (84)
+ * Enhance object literals (85)
  */
-
-// Реализовать методы увеличения и уменьшения баланса,
-// при котором кфждая операция сохраняется в массив
-// operations в виде { reason: 'Оплата налогов', sum: -100 }.
-// Возвращается true, если успешно и false, если не хватает баланса
-// Так же реализовать метод вывода числа операций по кошельку
 
 const wallet = {
     balance: 0,
     operations: [],
 
-    // Мое решение:
-    profit: function(reason, sum) {
-        this.balance += sum;
-        this.operations.push({reason: reason, sum: sum});
-        return true;
-    },
-
-    // Решение преподавателя:
     increase: function(sum, reason) {
         this.balance += sum;
         this.operations.push({
-            reason: reason,
-            sum: sum
+            reason, // название ключа и переменной со значением совапдают
+            sum // название ключа и переменной со значением совапдают
         });
         return true;
     },
 
-    // Мое решение:
-    spending: function(reason, sum) {
-        if (this.balance < sum) {
-            return false;
-        }
-        this.balance -= sum;
-        this.operations.push({reason: reason, sum: sum * -1});
-        return true;
-    },
-    // Решение преподавателя:
     decrease: function(sum, reason) {
         if (this.balance < sum) {
             console.log('Недостаточно баланса');
@@ -48,25 +24,19 @@ const wallet = {
         }
         this.balance -= sum;
         this.operations.push({
-            reason: reason,
+            reason, // название ключа и переменной со значением совапдают
             sum: -sum
         });
         return true;
     },
-    
-    // Мое решение:
-    viewOperation: function() {
-        return this.operations.length;
-    },
 
-    // Решение преподавателя:
     getOperationLenght: function() {
         return this.operations.length
     }
 };
 
-console.log(wallet.profit('Зарплата', 100));
-console.log(wallet.spending('Оплата налогов', 50));
-console.log(wallet.viewOperation());
+console.log(wallet.increase(100, 'Зарплата'));
+console.log(wallet.decrease(50, 'Оплата налогов'));
+console.log(wallet.getOperationLenght());
 
 console.log(wallet.balance);
