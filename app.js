78f23a25c0490a2f722aa7ea1aa0e91e8a70ded7
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-12 20:55:56 +0300

    Объекты. Упражнение - кошелек (84)

--- app.js
+++ app.js
@@ -1,26 +1,72 @@
 /**
  * Angol. 2024.07.12
  * Объекты
- * Методы объектов (83)
+ * Упражнение - кошелек (84)
  */
 
-const user = {
-    name: 'Вася',
-    surname: 'Пупкин',
-    age: 24,
+// Реализовать методы увеличения и уменьшения баланса,
+// при котором кфждая операция сохраняется в массив
+// operations в виде { reason: 'Оплата налогов', sum: -100 }.
+// Возвращается true, если успешно и false, если не хватает баланса
+// Так же реализовать метод вывода числа операций по кошельку
 
-    getFullName: function() {
-        console.log(this);
-        return this.name + ' ' + this.surname;
+const wallet = {
+    balance: 0,
+    operations: [],
+
+    // Мое решение:
+    profit: function(reason, sum) {
+        this.balance += sum;
+        this.operations.push({reason: reason, sum: sum});
+        return true;
+    },
+
+    // Решение преподавателя:
+    increase: function(sum, reason) {
+        this.balance += sum;
+        this.operations.push({
+            reason: reason,
+            sum: sum
+        });
+        return true;
+    },
+
+    // Мое решение:
+    spending: function(reason, sum) {
+        if (this.balance < sum) {
+            return false;
+        }
+        this.balance -= sum;
+        this.operations.push({reason: reason, sum: sum * -1});
+        return true;
+    },
+    // Решение преподавателя:
+    decrease: function(sum, reason) {
+        if (this.balance < sum) {
+            console.log('Недостаточно баланса');
+            return false;
+        }
+        this.balance -= sum;
+        this.operations.push({
+            reason: reason,
+            sum: -sum
+        });
+        return true;
+    },
+    
+    // Мое решение:
+    viewOperation: function() {
+        return this.operations.length;
+    },
+
+    // Решение преподавателя:
+    getOperationLenght: function() {
+        return this.operations.length
     }
-}
+};
 
-const getFullName = function(user) {
-    return user.name + ' ' + user.surname;
-}
+console.log(wallet.profit('Зарплата', 100));
+console.log(wallet.spending('Оплата налогов', 50));
+console.log(wallet.viewOperation());
 
-console.log(user.getFullName()); // обращение к методу объекта
-
-const arr = [1, 2];
-arr.sort(); // обращение к методу массива (объекта)
-'23432EFCD324'.toLowerCase(); // обращение к методу строки (объекта)
+console.log(wallet.balance);
