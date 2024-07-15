8a2a787463fed3c341bb75b96ed614db3ad633c8
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-15 18:24:24 +0300

    Объекты. Итерирование по объектам (86)

--- app.js
+++ app.js
@@ -1,42 +1,36 @@
 /**
  * Angol. 2024.07.12
  * Объекты
- * Enhance object literals (85)
+ * Итерирование по объектам (86)
  */
 
-const wallet = {
-    balance: 0,
-    operations: [],
-
-    increase: function(sum, reason) {
-        this.balance += sum;
-        this.operations.push({
-            reason, // название ключа и переменной со значением совапдают
-            sum // название ключа и переменной со значением совапдают
-        });
-        return true;
+// объект городов: город, температура, что-нибудь еще.
+const cities = {
+    msk: {
+        temp: 25
     },
-
-    decrease: function(sum, reason) {
-        if (this.balance < sum) {
-            console.log('Недостаточно баланса');
-            return false;
-        }
-        this.balance -= sum;
-        this.operations.push({
-            reason, // название ключа и переменной со значением совапдают
-            sum: -sum
-        });
-        return true;
-    },
-
-    getOperationLenght: function() {
-        return this.operations.length
+    spb: {
+        temp: 20
     }
-};
+}
 
-console.log(wallet.increase(100, 'Зарплата'));
-console.log(wallet.decrease(50, 'Оплата налогов'));
-console.log(wallet.getOperationLenght());
+// for (const city of cities) {
+//     console.log(city);
+// }
 
-console.log(wallet.balance);
+let sumTemp = 0;
+// let citiesCount = 0; // оптимизируем код
+let citiesCount = Object.keys(cities).length;
+// метод keys объекта Object позволяет получить ключи из нашего объекта
+
+for (const key in cities) {
+    // citiesCount ++; // оптимизируем код
+    sumTemp += cities[key].temp;
+}
+console.log(sumTemp / citiesCount);
+
+sumTemp = 0;
+for (const key of Object.keys(cities)) {
+    sumTemp += cities[key].temp;
+}
+console.log(sumTemp / citiesCount);
