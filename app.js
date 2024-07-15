989f44a8c8a48f28d0004babb4def9b9c80ae37f
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-15 21:25:01 +0300

    Объекты. Optional caining (88)

--- app.js
+++ app.js
@@ -1,46 +1,24 @@
 /**
  * Angol. 2024.07.12
  * Объекты
- * Деструктуризация и rest (87)
+ * Optional chaining (88)
  */
 
-// вспомним деструктуризацию массива.
-// деструктуризация массива позволяет разбить массив на отдельные переменные
-const arr = [1, 2, 3];
-const [z, y, x] = arr;
-console.log(z); // 1
-console.log(y); // 2
-console.log(x); // 3
+const cities = {
+    msk: {
+        temp: {
+            celcius: 25
+        }
+    },
+    spb: {
 
-// объекты
-let user = {
-    name: 'Вася',
-    age: 40,
-    city: 'Москва'
-};
+    }
+}
 
-// const {age, name} = user;
-// console.log(age); // 40
-// console.log(name); // Вася
+const city = 'krd';
 
-const {age, ...userWithouAge} = user;
-// возраст вытащили в одну переменную,
-// остальное в другой объект (rest)
-// можно оставить только, например, возраст, а все остальное не брать
-// const {age};
-console.log(userWithouAge);
+if (cities[city] !== undefined && cities[city].temp !== undefined) {
+    console.log(cities[city].temp.celcius);
+}
 
-const additionalData = {
-    skills: ['Разработка', 'Дизайн'],
-    creditCard: '1234-4567-9876-1234'
-};
-
-// user.skills = additionalData.skills;
-// user.creditCard = additionalData.creditCard;
-// воспользщуемся спред оператором
-
-user = {
-    ...user, // взять все свойства и переместить их в отдельную переменную
-    ...additionalData // взять все свойства и переместить их в отдельную переменную
-};
-console.log(user);
+console.log(cities[city]?.temp?.celcius); // undefined
