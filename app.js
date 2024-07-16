162231298d2fbb3331afd5e183a75bf48c856ea9
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-16 20:50:59 +0300

    Объекты. Упражнение - склад (89)

--- app.js
+++ app.js
@@ -1,24 +1,63 @@
 /**
  * Angol. 2024.07.12
  * Объекты
- * Optional chaining (88)
+ * Упражнение - склад (89)
  */
 
-const cities = {
-    msk: {
-        temp: {
-            celcius: 25
-        }
+// Сделать объект склад с методами, добавления на склад, поиска
+// по складу ьлваоа и оасчеь веса.
+
+const warehouse = {
+    goods: [],
+    findGoodById: function(id) {
+        return this.goods.find(g => g.id === id);
     },
-    spb: {
 
+    addGood: function(good) {
+        // const existeGood = this.goods.find(g => g.id === good.id);
+        const existeGood = this.findGoodById(good.id);
+        if (existeGood) {
+            console.log('Этот товар уже есть на складе');
+            return;
+        }
+        this.goods.push(good)
+    },
+    getMeightKg: function() {
+        return this.goods.reduce((acc, el) => 
+            acc += el.weight?.kg ? el.weight.kg : 0,
+            0
+        )
     }
+};
+
+// товары
+const car = {
+    id: 1,
+    weight: {
+        kg: 1000
+    },
+    barnd: 'Ford'
+};
+const chair = {
+    id: 2,
+    weight: {
+        kg: 2
+    },
+};
+const paper = {
+    id: 3,
+    color: 'red',
 }
+warehouse.addGood(car);
+warehouse.addGood(car);
+warehouse.addGood(chair);
+warehouse.addGood(paper);
+console.log(warehouse.goods);
 
-const city = 'krd';
+let findedItem = warehouse.findGoodById(3);
+console.log(findedItem);
+findedItem = warehouse.findGoodById(6);
+console.log(findedItem);
 
-if (cities[city] !== undefined && cities[city].temp !== undefined) {
-    console.log(cities[city].temp.celcius);
-}
-
-console.log(cities[city]?.temp?.celcius); // undefined
+const w = warehouse.getMeightKg();
+console.log(w);
