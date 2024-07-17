e407cae7cae40c0372762f470183ced98c27881c
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-17 21:04:52 +0300

    Как работает JS под капотом. Пример работы call stack (94)

--- app.js
+++ app.js
@@ -1,63 +1,22 @@
 /**
- * Angol. 2024.07.12
- * Объекты
- * Упражнение - склад (89)
+ * Angol. 2024.07.16
+ * Как работает JS под капотом
+ * Пример работы call stack (94)
  */
 
-// Сделать объект склад с методами, добавления на склад, поиска
-// по складу ьлваоа и оасчеь веса.
+const sum = 1;
+console.log('started');
+console.log(sum);
 
-const warehouse = {
-    goods: [],
-    findGoodById: function(id) {
-        return this.goods.find(g => g.id === id);
-    },
-
-    addGood: function(good) {
-        // const existeGood = this.goods.find(g => g.id === good.id);
-        const existeGood = this.findGoodById(good.id);
-        if (existeGood) {
-            console.log('Этот товар уже есть на складе');
-            return;
-        }
-        this.goods.push(good)
-    },
-    getMeightKg: function() {
-        return this.goods.reduce((acc, el) => 
-            acc += el.weight?.kg ? el.weight.kg : 0,
-            0
-        )
+function add5(n) {
+    n = n + 5;
+    if (n > 100) {
+        return n;
     }
-};
 
-// товары
-const car = {
-    id: 1,
-    weight: {
-        kg: 1000
-    },
-    barnd: 'Ford'
-};
-const chair = {
-    id: 2,
-    weight: {
-        kg: 2
-    },
-};
-const paper = {
-    id: 3,
-    color: 'red',
+    console.log(n);
+    return add5(n);
 }
-warehouse.addGood(car);
-warehouse.addGood(car);
-warehouse.addGood(chair);
-warehouse.addGood(paper);
-console.log(warehouse.goods);
 
-let findedItem = warehouse.findGoodById(3);
-console.log(findedItem);
-findedItem = warehouse.findGoodById(6);
-console.log(findedItem);
-
-const w = warehouse.getMeightKg();
-console.log(w);
+add5(sum);
+console.log('ended');
