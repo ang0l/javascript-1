3aea03d34a7f2f939efb4eb1e8d4adba960ccb5e
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-18 21:09:23 +0300

    Scope и this. Пример использования this (103)

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
 /**
  * Angol. 2024.07.18
  * Scope и this
- * Ключевое слово this (102)
+ * Пример использования this (103)
  */
 
-// 'use strict';
+'use strict';
 
-// Куда уазывает this
-//
-// в методе - на объект этого метода
-// в функции - undefined
-// в стрелочной фуккции - на родительский скоп
-// event listener - на DOM-элемент дерева, к которому они прикреплены
+console.log(window); // Window
+console.log(this); // Window
+
+function addNum(num1, num2) {
+    console.log(this);
+    return num1 + num2;
+}
+addNum(); // undefined
+
+const addNum2 = (num1, num2) => {
+    console.log(this);
+    return num1 + num2;
+}
+addNum2(); // Window
+
+const user = {
+    name: 'Вася',
+    surname: 'Пупкин',
+    getFulName: function() {
+        console.log(this);
+        return this.name + ' ' + this.surname;
+    }
+}
+user.getFulName(); // {name: 'Вася', surname: 'Пупкин', getFulName: ƒ}
+
+const user2 = {
+    name: 'Марина',
+    surname: 'Катц',
+}
+user2.getFulName = user.getFulName;
+user2.getFulName(); // {name: 'Марина', surname: 'Катц', getFulName: ƒ}
+
+const getFulName = user2.getFulName;
+getFulName(); // undefined и ошибка о невозможности прочитать свойства name и surname
