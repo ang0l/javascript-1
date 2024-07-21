244fa5c488e178ea793f3a49aa969e221319b6ee
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-21 14:59:34 +0300

    Управление this. Call, apply (108)

--- app.js
+++ app.js
@@ -1,33 +1,45 @@
 /**
- * Angol. 2024.07.20
+ * Angol. 2024.07.21
  * Управление this
- * EOL для методов (108)
+ * Call, apply (108)
  */
 
 'use strict';
 
-// небольшое напоминание
-// const b = 1;
-// const a = {
-//     b
-// }
-
-// console.log(a); // {b: 1}
-
-const b = 1;
-const a = {
-    b,
-    getB: function() {
-        return this.b;
-    },
-
-    // следующая запись метода (getBAlt) делает абсолютно тоже, что и метод getB
-    // в объектах лучше методы писать именно так
-    // в классах методы пишутся так же:
-    getBAlt() {
-        return this.b;
+const audi = {
+    make: 'Audi',
+    model: 'A3',
+    year: 2021,
+    damages: [],
+    addDamage(part, rate) {
+        console.log(`У авто ${this.make} ${this.model} ${this.year} добавлено: повреждение ${part} со степенью ${rate}`); // У авто Audi A3 2021 добавлено: повреждение Капот со степенью 1
+        this.damages.push({
+            part,
+            rate,
+        })
     }
-}
+};
 
-console.log(a.getB()); // 1
-console.log(a.getBAlt()); // 1
+audi.addDamage('Капот', 1);
+
+const bmw = {
+    make: 'BMW',
+    model: 'X5',
+    year: 2022,
+    damages: [],
+};
+
+bmw.addDamage = audi.addDamage;
+bmw.addDamage('Бампер', 2); // У авто BMW X5 2022 добавлено: повреждение Бампер со степенью 2
+
+const addDamageFunc = audi.addDamage;
+// addDamageFunc('Бампер', 2); // ошибка
+
+// мы хотим вызвать функцию addDamageFunc в контексте объекта bmw
+// и передав в эту функцию 2 аргумента: Бампер и 2:
+addDamageFunc.call(bmw, 'Бампер', 2); // У авто BMW X5 2022 добавлено: повреждение Бампер со степенью 2
+addDamageFunc.call(audi, 'Бампер', 2); // У авто Audi A3 2021 добавлено: повреждение Бампер со степенью 2
+
+// мы хотим применить метод объекта с массивом аргументов
+addDamageFunc.apply(bmw, ['Дверь', 3]); // У авто BMW X5 2022 добавлено: повреждение Дверь со степенью 3
+addDamageFunc.apply(audi, ['Дверь', 3]); // У авто Audi A3 2021 добавлено: повреждение Дверь со степенью 3
