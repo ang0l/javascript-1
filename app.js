ae34a13fc602726c2ee61854c3a38e6df012950b
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-22 06:12:36 +0300

    Управление this. Bind (109)

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /**
  * Angol. 2024.07.21
  * Управление this
- * Call, apply (108)
+ * Bind (109)
  */
 
 'use strict';
@@ -9,37 +9,22 @@
 const audi = {
     make: 'Audi',
     model: 'A3',
-    year: 2021,
     damages: [],
+};
+
+const carManipulation = {
     addDamage(part, rate) {
-        console.log(`У авто ${this.make} ${this.model} ${this.year} добавлено: повреждение ${part} со степенью ${rate}`); // У авто Audi A3 2021 добавлено: повреждение Капот со степенью 1
-        this.damages.push({
-            part,
-            rate,
-        })
+        this.damages.push({part, rate});
+        console.log(`Добавить повреждение на ${this.make} ${this.model}`);
     }
 };
 
-audi.addDamage('Капот', 1);
+// bind говорит о том, что метод addDamage связан с объектом audi
+// кодга мы будем вызывать addDamage, this будет принимать значение нашего audi
+const addDamabeAudi = carManipulation.addDamage.bind(audi);
+addDamabeAudi('Крыло', 3);
+console.log(audi);
 
-const bmw = {
-    make: 'BMW',
-    model: 'X5',
-    year: 2022,
-    damages: [],
-};
-
-bmw.addDamage = audi.addDamage;
-bmw.addDamage('Бампер', 2); // У авто BMW X5 2022 добавлено: повреждение Бампер со степенью 2
-
-const addDamageFunc = audi.addDamage;
-// addDamageFunc('Бампер', 2); // ошибка
-
-// мы хотим вызвать функцию addDamageFunc в контексте объекта bmw
-// и передав в эту функцию 2 аргумента: Бампер и 2:
-addDamageFunc.call(bmw, 'Бампер', 2); // У авто BMW X5 2022 добавлено: повреждение Бампер со степенью 2
-addDamageFunc.call(audi, 'Бампер', 2); // У авто Audi A3 2021 добавлено: повреждение Бампер со степенью 2
-
-// мы хотим применить метод объекта с массивом аргументов
-addDamageFunc.apply(bmw, ['Дверь', 3]); // У авто BMW X5 2022 добавлено: повреждение Дверь со степенью 3
-addDamageFunc.apply(audi, ['Дверь', 3]); // У авто Audi A3 2021 добавлено: повреждение Дверь со степенью 3
+const addDamabeAudiRoof = carManipulation.addDamage.bind(audi, 'Крыша');
+addDamabeAudiRoof(5);
+console.log(audi);
