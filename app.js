9f682782728fd52bd350ff9217b02093d79dce9e
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-23 06:19:59 +0300

    Управление this. Bind (110)

--- app.js
+++ app.js
@@ -1,30 +1,44 @@
 /**
- * Angol. 2024.07.21
+ * Angol. 2024.07.22
  * Управление this
- * Bind (109)
+ * Bind (110)
  */
 
 'use strict';
 
-const audi = {
-    make: 'Audi',
-    model: 'A3',
-    damages: [],
-};
+// Создайте объект пользователя с паролем
+// С помощью функции ниже удалить пароль сделав
+// фнкуцию сброса пароля
 
-const carManipulation = {
-    addDamage(part, rate) {
-        this.damages.push({part, rate});
-        console.log(`Добавить повреждение на ${this.make} ${this.model}`);
+function removePassword(reset) {
+    if (reset) {
+        this.password = undefined;
+    } else {
+        this.password = 1;
     }
+}
+
+// Мое решение
+const user = {
+    name: 'Vasya',
+    login: 'VP',
+    password: 2,
 };
 
-// bind говорит о том, что метод addDamage связан с объектом audi
-// кодга мы будем вызывать addDamage, this будет принимать значение нашего audi
-const addDamabeAudi = carManipulation.addDamage.bind(audi);
-addDamabeAudi('Крыло', 3);
-console.log(audi);
+console.log(user.password);
 
-const addDamabeAudiRoof = carManipulation.addDamage.bind(audi, 'Крыша');
-addDamabeAudiRoof(5);
-console.log(audi);
+const remPass = removePassword;
+remPass.call(user, true)
+
+console.log(user.password);
+
+// решение преподавателя
+
+const user1 = {
+    login: 'a@a.ru',
+    password: '12345',
+};
+
+const resetUserPassword = removePassword.bind(user1, true);
+resetUserPassword();
+console.log(user1.password);
