41a2a6de68abf45b7769f9f285d58ffaac7134bf
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-26 08:52:50 +0300

    Управление this. Упражнение - Работа с замыканиями (113)

--- app.js
+++ app.js
@@ -1,32 +1,69 @@
 /**
  * Angol. 2024.07.24
  * Управление this
- * Замыкания (112)
+ * Упражнение - Работа с замыканиями (113)
  */
 
 'use strict';
 
-function changeBalance() {
-    let balance = 0;
-    let key = 'abc';
-    return function(sum) {
-        balance += sum;
-        key = 'cba';
-        console.log(`Баланс: ${balance}`);
+// сделать функцию пользователя, которая берет за основу
+// userInfo и за счет замыкания создает новый объект, с 
+// которым можно работать как user1().increse(100)
+
+const userInfo = {
+    balanse: 0,
+    ooperations: 0,
+    increse(sum) {
+        this.balanse += sum;
+        this.ooperations ++;
+    },
+};
+
+// Мое решение
+const userMy = userInfo;
+
+userMy.increse(100);
+userMy.increse(-50);
+userMy.increse(200);
+
+console.log(userMy.balanse); // 250
+
+// вобщем я замыкание вообще не использоват.
+// если я правльно понял, то замыкание - это функция возвращающая функцию
+// бррр. запутался. пересмотрю позже.
+
+// Решение преподавателя
+// function user() {
+//     const userObj = userInfo;
+//     return function() {
+//         return userObj;
+//     }
+// }
+// Данный (выше) код делает ссылку на объект. все изменения проиходят в одном объекте
+// Чтобы этого избежать, необходимо каждый раз создавать новый объект:
+function user() {
+    const userObj = {
+        balanse: 0,
+        ooperations: 0,
+        increse(sum) {
+            this.balanse += sum;
+            this.ooperations ++;
+        }
+    }
+    return function() {
+        return userObj;
     }
 }
 
-const change = changeBalance();
 
-change(100); // 100
-change(-50); // 50
-change(200); // 250
+const user1 = user();
 
-console.dir(change);
+user1().increse(100);
+user1().increse(100);
+console.log(user1());
 
+const user2 = user();
 
-const change2 = changeBalance();
+user2().increse(100);
+console.log(user2());
 
-change2(100); // 100
-
-console.dir(change2);
