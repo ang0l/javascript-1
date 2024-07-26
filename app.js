873f3900548e0494fafd60cb9270fe6e81234d3f
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-26 17:40:43 +0300

    DOM. Установка атрибутов (119)

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /**
  * Angol. 2024.07.26
  * DOM
- * Работа со стилями и классами (118)
+ * Установка атрибутов (119)
  */
 
 'use strict';
@@ -15,24 +15,15 @@ function submitForm() {
     document.querySelector('.panel').innerText = input;
     document.querySelector('.input').value = '';
 
-    // вариант работы со стилями:
-    // вариант плох тем, что необходимо руками редактирвать каждое свойство
-    // document.querySelector('.notification').style.display = 'block';
-
-    // вариант работы с классами:
-    // добавляем класс
     // document.querySelector('.notification').classList.add('notification_active')
-    // точка перед классом нужна в querySelector
-    // и  не нужна при добавлении класса, 
-    // т.к. мы добавляем непосредственно конкретное наименование класса в список добовления
-    // через запятую
+    // document.querySelector('.notification').classList.remove('notification_hidden')
 
-    // удаляем класс
-    // добавляем конкретныее наименования классов в список удаления, через запятую.
-    // точка также не нужна
-    document.querySelector('.notification').classList.remove('notification_hidden')
-
-    console.log();
+    console.log(document.querySelector('.notification').getAttribute('class'));
+    document.querySelector('.notification').setAttribute('class', 'notification');
+    document.querySelector('.notification').setAttribute('key', 1);
+    document.querySelector('.notification').setAttribute('user-id', 1);
+    // атрибут - всегда строковое значение
+    console.log(Number(document.querySelector('.notification').getAttribute('user-id')));
 }
 
 function inputChanged(e) {
