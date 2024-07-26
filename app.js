58fa4e45d81a0156227324718d691923dded49c8
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-26 18:09:30 +0300

    DOM. Упражнение - получение DOM элементов (120)

--- app.js
+++ app.js
@@ -1,33 +1,22 @@
 /**
  * Angol. 2024.07.26
  * DOM
- * Установка атрибутов (119)
+ * Упражнение - получение DOM элементов (120)
  */
 
 'use strict';
 
-function submitForm() {
-    const input = document.querySelector('.input').value;
-    if (!input) {
-        return;
-    }
+console.log('Мое решение');
+console.log(document.querySelector('.one').innerText);
+console.log(document.querySelector('#two').innerText);
 
-    document.querySelector('.panel').innerText = input;
-    document.querySelector('.input').value = '';
+console.log('Решение преподавателя');
+console.log(document.querySelector('.one').innerText);
+console.log(document.querySelector('.one ~ div').innerText); // не совсем уместно, т.к. div может быть не соседний
+console.log(document.querySelector('#two').innerText);
+console.log(document.querySelector('[user-id="4"]').innerText);
 
-    // document.querySelector('.notification').classList.add('notification_active')
-    // document.querySelector('.notification').classList.remove('notification_hidden')
-
-    console.log(document.querySelector('.notification').getAttribute('class'));
-    document.querySelector('.notification').setAttribute('class', 'notification');
-    document.querySelector('.notification').setAttribute('key', 1);
-    document.querySelector('.notification').setAttribute('user-id', 1);
-    // атрибут - всегда строковое значение
-    console.log(Number(document.querySelector('.notification').getAttribute('user-id')));
-}
-
-function inputChanged(e) {
-    if (e.code == 'Enter') {
-        submitForm();
-    }
-}
+console.log('Альтернативные пути получения контента:');
+console.log(document.querySelectorAll('.one')[0].innerText);
+console.log(document.querySelectorAll('.one')[1].innerText);
+console.log(document.getElementById('two').innerText); // точка не нужна
