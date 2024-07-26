a965ea81910dae4fbcc99c2b7cc7495456b8e1ed
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-26 16:45:45 +0300

    DOM. Обработка событий клавиатуры (117)

--- app.js
+++ app.js
@@ -1,26 +1,12 @@
 /**
  * Angol. 2024.07.26
  * DOM
- * Обработка нажатий (116)
+ * Обработка событий клавиатуры (117)
  */
 
 'use strict';
 
-document.querySelector('.button').addEventListener('click', function(e) {
-    // console.log(e); // в браузере при клике на кнопку, в консоле выводится poineterEvent
-    const input = document.querySelector('.input').value;
-    if (!input) {
-        return;
-    }
-
-    // console.log(input); // проверяем, что получилось
-
-    document.querySelector('.panel').innerText = input; // меняем текст в панели
-    document.querySelector('.input').value = ''; // очищаем поле ввода
-});
-
-// сделаем все немного красивие:
-function changeClick() {
+function submitForm() {
     const input = document.querySelector('.input').value;
     if (!input) {
         return;
@@ -28,7 +14,23 @@ function changeClick() {
 
     document.querySelector('.panel').innerText = input;
     document.querySelector('.input').value = '';
-};
+}
 
-// как вызвать?
-// вызовем из HTML-кода (строка 14): <button class="button" onclick="changeClick">Change</button>
+function changeClick(e) {
+    if (e.code == 'Enter') {
+        submitForm();
+    }
+}
+
+// вышенаписанный код на много чище ниженаписанного
+// внизу хоть и написано какие селекторы затрагиваются,
+// но их видно в HTML событиями
+// и их можно посмотреть провалившись в app.js
+
+// document.querySelector('.input').addEventListener('keydown', (e) => {
+//     // console.log(e);
+//     if (e.code == 'Enter') {
+//         // console.log('Enter');
+//         submitForm();
+//     }
+// })
