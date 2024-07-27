5721cbfb84d20c210258cdb04cb9e95b808921bf
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-27 08:41:24 +0300

    DOM. Упражнение - сохранение JSON (124)

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 /**
  * Angol. 2024.07.27
  * DOM
- * JSON (123)
+ * Упражнение - сохранение JSON (124)
  */
 
 'use strict';
+
+// key: 'text, value: {text:'sdfsdf'}
 
 function submitForm() {
     const input = document.querySelector('.input').value;
@@ -14,9 +16,25 @@ function submitForm() {
 
     document.querySelector('.panel').innerText = input;
     document.querySelector('.input').value = '';
-    document.querySelector('.notification').classList.remove('notification_hidden')
+    document.querySelector('.notification').classList.remove('notification_hidden');
 
-    console.log();
+    // Мое решение. Начало
+    const objText = {
+        text: input,
+    }
+
+    const text = JSON.stringify(objText);
+    localStorage.setItem('text', text);
+    // Мое решение. Конец
+
+    // Решение преподавателя. Начало
+    const textString = JSON.stringify({
+        text: input,
+    });
+    localStorage.setItem('text', textString);
+    // Решение преподавателя. Конец
+
+    // Объект создается непосредственно при создании JSON
 }
 
 function inputChanged(e) {
@@ -24,10 +42,3 @@ function inputChanged(e) {
         submitForm();
     }
 }
-
-const obj = JSON.parse('{"a": 1}');
-console.log(obj); // объект JSON
-console.log(obj.a); // элемент объекта JSON
-
-const str = JSON.stringify(obj);
-console.log(str); // строка содержащая JSON. но при этом - строка.
