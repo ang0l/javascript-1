88c22b5e3d09dc01f27779b35080f0b136abc305
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2024-07-28 11:41:54 +0300

    Проект Трекер привычек. Загрузка и соъхранение данных (129)

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,31 @@
 /**
- * Angol. 2024.07.27
+ * Angol. 2024.07.28
  * Проект "Трекер привычек"
- * Верстка макета - 2 (128)
+ * Загрузка и сохранение данных (129)
  */
 
 'use strict';
+
+let habbits = [];
+const HABBIT_KEY = 'HABBIT_KEY';
+
+// utils
+
+function loadData() {
+    const habbitString = localStorage.getItem(HABBIT_KEY);
+    const habbitArray = JSON.parse(habbitString);
+    if (Array.isArray(habbitArray)) {
+        habbits = habbitArray;
+    }
+}
+
+function saveData() {
+    localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
+}
+
+// дополнительно к запуску функции
+// в localStorage были загружены данные
+// из ../data/demo.json
+(() => {
+    loadData();
+})()
